2f781772a7d8fbf8964221f423da8d65a06ed92c
cjaguinaldo <cjaguinaldo@outlook.com>
2022-06-17 00:36:49 +0800

    added internal routing

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,5 +1,14 @@
 <template >
-  <router-view></router-view>
+  <div>
+    <div>
+      <router-link to="video-test">Video</router-link> /
+      <router-link to="slide-test">Slide vs Scroll</router-link>
+    </div>
+    <router-view></router-view>
+
+
+  </div>
+
 </template>
 <script>
 export default {
